f07672098a26a5692a889a1889899a9ec33a408d
Ezequiel Orazi <112835932+Ezzeorazi@users.noreply.github.com>
2023-05-29 18:28:51 -0300

    Add files via upload

---
+++ ejercicioNumber.js
@@ -0,0 +1,22 @@
+let alturaInt = 192;
+let alturaFloat = 1.92;
+let peso = 98.5;
+var alturaUp = alturaFloat.toFixed()
+var pesoDown = parseInt(peso)
+
+
+
+console.log(typeof(alturaInt))
+console.log(typeof(alturaFloat))
+console.log(alturaUp)
+console.log(pesoDown)
+
+let max_valor1_JS = Number.MAX_VALUE + 1;
+let max_valor_JS = Number.MAX_VALUE;
+
+let resultado = max_valor1_JS === max_valor_JS;
+
+console.log(resultado)
+
+
+
