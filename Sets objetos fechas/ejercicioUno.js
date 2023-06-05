1e6d1cbfa5a521ff7e635fda066ee1e0ca5d2296
Ezequiel Orazi <112835932+Ezzeorazi@users.noreply.github.com>
2023-06-05 17:43:21 -0300

    Add files via upload

---
+++ Sets objetos fechas/ejercicioUno.js
@@ -0,0 +1,6 @@
+const familia = ["Marianela", "Tony", "Lujan", "Ezequiel", "Mili"];
+const miFamiliaSet = new Set(familia);
+miFamiliaSet.add("Lujan");
+miFamiliaSet.add("Javascript");
+console.log(miFamiliaSet)
+
