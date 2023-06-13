f39ec94277ed4fdf9a4258d8e867cf5989bc7226
Ezequiel Orazi <112835932+Ezzeorazi@users.noreply.github.com>
2023-06-12 23:28:11 -0300

    Add files via upload

---
+++ controller/index.js
@@ -0,0 +1,12 @@
+import { suma, multiplica } from './modulos/controller.js'
+import chalk from 'chalk';
+
+const sum = suma(5,12)
+console.log(sum)
+
+const mult = multiplica(2,21)
+console.log(mult)
+
+
+
+console.log(chalk.green('Hello world!'));
